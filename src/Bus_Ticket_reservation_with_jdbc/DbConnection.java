40826a4d2147b188aa23be267afbb63f2cd6ede1
Boobathi776 <146323729+Boobathi776@users.noreply.github.com>
2024-11-24 09:08:05 +0530

    Update DbConnection.java

--- src/Bus_Ticket_reservation_with_jdbc/DbConnection.java
+++ src/Bus_Ticket_reservation_with_jdbc/DbConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 
 public class DbConnection {
     private static final String url= "jdbc:mysql://localhost:3306/busresv";
-    private static final String username="root";
-    private static final String pass="Boobathi776##";
+    private static final String username="xxxxxxxxxx";
+    private static final String pass="xxxxxxxxxxxxxxxxxxxx";
     public static Connection getConnection() throws Exception  // this method going to return the connection object
     {                                                          // so return type  Connection class
         return DriverManager.getConnection(url,username,pass);
